429656186cb4c197049fe1d56aeace1e1a3fc5fb
Francisco Colino <francisco.m.colino@gmail.com>
2023-06-25 23:52:12 +0100

    Add experimental server and client

---
+++ experiments/exp_client.py
@@ -0,0 +1,19 @@
+import asyncio
+
+import websockets
+
+PORT = 8765
+HOST = "localhost"
+
+async def client():
+    server_address = "ws://" + HOST + ":" + str(PORT)
+    async with websockets.connect(server_address) as websocket:
+        # Send messages to the server
+        await websocket.send("Hello, server!")
+
+        # Receive and print the server's response
+        response = await websocket.recv()
+        print(f"Received response from server: {response}")
+
+# Run the client
+asyncio.get_event_loop().run_until_complete(client())
---
+++ experiments/exp_server.py
@@ -0,0 +1,20 @@
+import asyncio
+
+import websockets
+
+PORT = 8765
+
+async def server(websocket, path):
+    # Handle incoming messages from the client
+    async for message in websocket:
+        print(f"Received message from client: {message}")
+        
+        # Echo the message back to the client
+        await websocket.send(f"Server received: {message}")
+
+# Start the server
+start_server = websockets.serve(server, "localhost", PORT)
+
+# Run the event loop
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
